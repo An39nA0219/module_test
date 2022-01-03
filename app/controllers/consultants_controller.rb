7aa4d080baa1f5e9f47ff80ca9402ea29f7ff143
An39nA0219 <an39na10282004@yahoo.co.jp>
2022-01-03 10:29:23 +0900

    add nickname

--- app/controllers/consultants_controller.rb
+++ app/controllers/consultants_controller.rb
@@ -1,5 +1,6 @@
 class ConsultantsController < ApplicationController
   def index
     @consultants = Consultant.all
+    @consultant_nicknames = @consultants.map{|consultant| consultant.name + "っち"}
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @user_nicknames = @users.map{|user| user.name + "っち"}
   end
 end
