74dddc4d1287a73da1c1d40171a0a079b512c51e
An39nA0219 <an39na10282004@yahoo.co.jp>
2022-01-03 09:18:52 +0900

    create users model & consultants model

---
+++ db/migrate/20220103001051_create_consultants.rb
@@ -0,0 +1,10 @@
+class CreateConsultants < ActiveRecord::Migration[7.0]
+  def change
+    create_table :consultants do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
